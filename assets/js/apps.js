dda9aeedb911bc3a4aa871bfbcc41c54f800595a
OAN <oan@fedora.home>
2023-04-06 15:46:08 +0200

    fix: string capitalized. test : sorting between string and number

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,15 +1,41 @@
-// retrive the datas from the DOM
+// retrieve the datas from the DOM and capitalize them.
 
-const data = document.getElementById("statementData").innerHTML;
+const data = document.getElementById("statementData").innerHTML.toUpperCase();
 console.log(data);
+
+
+// const stringData = data.forEach(value) {
+//     if(value !== number ) {
+//         return string;
+//     }
+// };
+
+// data.valueOf = function (value) { 
+//     if (value === string
+
+// }
+
+
+
+
+// data.forEach(function letterSelected(data) {
+//     if (data.element == 
+// })
+
 
 const tableData = [... data];
 console.log(tableData);
 
-// const letterArray = tableData.filter(function letter() {
-//     if (String) {
-//         return value;
-//     }
-// });
+tableData.forEach( element => console.log(element));
+const str = tableData.forEach( element => (element) {
+    if(tableData.element !== number) {
+        return value;
+    }
+} );
 
-// console.log(letterArray);
+console.log(str);
+
+// const sortingData = tableData.sort();
+
+// console.log(sortingData);
+
