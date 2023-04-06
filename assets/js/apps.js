7586044f7b1ba6dcccb44f4d2d6bda85694a8aac
OAN <oan@fedora.home>
2023-04-06 15:54:13 +0200

    test : new function for sorting string and number => return undefined instead of string expected

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -4,34 +4,17 @@ const data = document.getElementById("statementData").innerHTML.toUpperCase();
 console.log(data);
 
 
-// const stringData = data.forEach(value) {
-//     if(value !== number ) {
-//         return string;
-//     }
-// };
-
-// data.valueOf = function (value) { 
-//     if (value === string
-
-// }
-
-
-
-
-// data.forEach(function letterSelected(data) {
-//     if (data.element == 
-// })
-
-
 const tableData = [... data];
 console.log(tableData);
 
 tableData.forEach( element => console.log(element));
-const str = tableData.forEach( element => (element) {
-    if(tableData.element !== number) {
+
+
+const str = tableData.forEach(element => function(value) {
+    if (value !== number) {
         return value;
     }
-} );
+});
 
 console.log(str);
 
