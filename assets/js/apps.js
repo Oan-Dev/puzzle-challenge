dbf4a2997387560a03698fcddae3f43fa303d3e0
OAN <oan@fedora.home>
2023-04-08 10:24:47 +0200

    test : different methods for removing all number elements typed string in array => fail

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -12,18 +12,46 @@ dataTable.forEach( element => console.log(element, typeof element));
 
 // retrieve only numbers converted from string to number type
 const numberValueTable = dataTable.map(element => Number(element)).filter(Number);
-console.log(numberValueTable);
+console.log(numberValueTable, typeof(numberValueTable.values));
+
+// adding number regex for checking number in number type ?
+const reg = new RegExp('^[0-9]+$');
+
+//deep copy from datatable for terminating number
+const stringArray = [...dataTable].filter(element => element !== numberValueTable);
+console.log(stringArray, typeof element);
+
+// console.log(reg.test(stringArray));
+// console.log(reg.test(numberValueTable));
+
 
 // retrieve only letters without numbers
-const letterValueTable = dataTable.map((element) => { 
 
-    if(element != numberValueTable) {
+// const stringLetter = stringArray.filter()
+
+
+
+
+// stringArray.forEach(item => (item) => {
+//     const stringLetter =  stringArray.map((item.typeof(String) !== numberValueTable));
+//     return console.log(stringLetter);
+
+// } );
+
+
+// console.log(stringArray);
+
+
+
+// const letterValueTable = dataTable.map((element) => { 
+
+//     if(element != numberValueTable) {
         
-        element.slice(element => {
-            return element
-        })
-        return 
-    }
-})
-console.log(letterValueTable);
+//         element.slice(element => {
+//             return element
+//         })
+//         return 
+//     }
+// })
+// console.log(letterValueTable);
 
