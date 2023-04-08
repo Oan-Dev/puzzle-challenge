f5d6c681d2142a73b058c4ee809fcbce455cb1de
OAN <oan@fedora.home>
2023-04-08 16:13:27 +0200

    issue : attempting for converting the typing of the array's numbers typed string in number type => fail

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -28,26 +28,45 @@ console.log("is Number's array elements of dataTable : ",isNumber(dataTable))
 
 numberValueTable.forEach( element => console.log(element, typeof element));
 
-// Separating Number and String
+
+/** 
+TEST for converting number typed from string to number type and
+ separating the real number and real string 
+
+/
+
+*/  
+
+//Separating Number and String
 const stringArray = [];
 const numberArray = [];
 
+const typedArray = parseInt([...dataTable])
+
+// typedArray.map(((element) => typeof element === 'string'))
+
+console.log(typedArray);
+
+
+const newArray = function filterArray(){
+    return dataTable.filter((element) => typeof element === 'string')
+}
+
+console.log(newArray);
+
 // console.log(parseInt(dataTable));
 
-dataTable.forEach(element => {
+// dataTable.forEach(element => {
+//     if(typeof(element !== typeof(parseInt(dataTable)))) {
+//         stringArray.push(element);
+//     }
+//     if(typeof(element === typeof(parseInt(dataTable)))) {
+//         numberArray.push(element);
+//     }
+// });
 
-  
-
-    if(typeof(element !== typeof(parseInt(dataTable)))) {
-        stringArray.push(element);
-    }
-    if(typeof(element === typeof(parseInt(dataTable)))) {
-        numberArray.push(element);
-    }
-});
-
-console.log(stringArray);
-console.log(numberArray);
+// console.log(stringArray);
+// console.log(numberArray);
 
 // console.log(reg.test(stringArray));
 // console.log(reg.test(numberValueTable));
