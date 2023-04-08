126047fef65420cf2cb2d3f1401a209cd28bd5de
OAN <oan@fedora.home>
2023-04-08 10:53:51 +0200

    test : checking a function returning a converting number typed number

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -12,14 +12,23 @@ dataTable.forEach( element => console.log(element, typeof element));
 
 // retrieve only numbers converted from string to number type
 const numberValueTable = dataTable.map(element => Number(element)).filter(Number);
-console.log(numberValueTable, typeof(numberValueTable.values));
+console.log(numberValueTable, typeof numberValueTable);
 
 // adding number regex for checking number in number type ?
 const reg = new RegExp('^[0-9]+$');
 
+
+// function for checking if array element is a number and return boolean
+const isNumber = function isNumber(n){
+    return Number(n)=== n;
+}
+
+console.log(isNumber(numberValueTable))
+
+
 //deep copy from datatable for terminating number
-const stringArray = [...dataTable].filter(element => element !== numberValueTable);
-console.log(stringArray, typeof element);
+// const stringArray = [...dataTable].filter(element => element !== isNumber(numberValueTable));
+// console.log(stringArray, typeof element);
 
 // console.log(reg.test(stringArray));
 // console.log(reg.test(numberValueTable));
