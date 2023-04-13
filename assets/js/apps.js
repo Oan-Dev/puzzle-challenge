0478856a207774a726b060f546ef328a06cc58a2
OAN <oan@fedora.home>
2023-04-13 18:33:01 +0200

    clean code : delete some code in comments

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -344,42 +344,6 @@ const preCode = pre.appendChild(code);
 
 /** INSERT CONTENTS TO THE P */
 
-
-// const joined = textContent.map((el, i) => {
-//     return (i % 2 === 0) ? `<h2>${el}</h2>` : `<p>${el}</p>` ;
-//   }).join('')
-  
-//   console.log(joined);
-
-// const joined = textContent.map((el, i) => {
-//     return (i % 2 === 0) ?  `<p>${el}</p>`: null ;
-//   }).join('')
-  
-//   console.log(joined);
-
-
-// const map1 = new Map();
-
-// map1.set('a', 1);
-// map1.set('b', 2);
-// map1.set('c', 3);
-
-// console.log(map1.get('a'));
-// // Expected output: 1
-
-// map1.set('a', 97);
-
-// console.log(map1.get('a'));
-// // Expected output: 97
-
-// console.log(map1.size);
-// // Expected output: 3
-
-// map1.delete('b');
-
-// console.log(map1.size);
-// // Expected output: 2
-
 function arrToPar(txtArr){ //For JS-Array --> HTML content
     
     for(var i = 0; i < txtArr.length; i++){
