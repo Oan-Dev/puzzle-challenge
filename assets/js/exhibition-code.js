00567dba4467d8986c347e12476172867bdac5ba
OAN <oan@fedora.home>
2023-04-22 18:05:59 +0200

    fix : transform each description of coding in obj and then ref in new array

--- assets/js/exhibition-code.js
+++ assets/js/exhibition-code.js
@@ -1,92 +1,101 @@
 
 /** Exhibition of  coding */
 
-
+const ExhibitingOfCoding = {
 // Section id 1
-const retrievingData = 'document.getElementById("data-localisation-id").innerHTML.toUpperCase()';
+ 'retrievingData' :                 'document.getElementById("data-localisation-id").innerHTML.toUpperCase()',
 
 // Section id 2
-const deepDataCopy = '[... data]';
+ 'deepDataCopy' :                   '[... data]',
 
 // Section id 3
-const mappingAndFiltering = 'data.map(element => Number(element)).filter(Number)';
+ 'mappingAndFiltering' :            'data.map(element => Number(element)).filter(Number)',
 
 // Section id 4
-const numberConvertedInString = `const numberArray = [];
+ 'numberConvertedInString' :            `const numberArray = [];
 
-for (i = 0; i < 10; i++) {
-    numberArray.push(i)
-}
+                                        for (i = 0; i < 10; i++) {
+                                            numberArray.push(i)
+                                        }
 
-for (i = 0; i < 10; i++) {
-    const numStr = numberArray[i].toString();
-    numberArray.push(numStr)
-}`;
+                                        for (i = 0; i < 10; i++) {
+                                            const numStr = numberArray[i].toString();
+                                            numberArray.push(numStr)
+                                        }`,
 
 // Section Id 5
-const comparatingDataWithNumberArray = 'const stringArray = data.filter(x => !numberArray.includes(x)).sort()';
+ 'comparatingDataWithNumberArray' :     'const stringArray = data.filter(x => !numberArray.includes(x)).sort()',
 
 // section Id 6
-const alphabetArrayAndComparaingWithStringArray = `var alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split('');
-var alphabetPosition = () => stringArray.map(x => alphabet.indexOf(x) + 1);
-const alphaPosition = alphabetPosition(stringArray);`;
+ 'alphabetArrayAndComparaingWithStringArray' : 
+                                        `var alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split('');
+                                        var alphabetPosition = () => stringArray.map(x => alphabet.indexOf(x) + 1);
+                                        const alphaPosition = alphabetPosition(stringArray);`,
 
 // Section id 7
 
-const splitingForRunningModulo = `alphaPosition.filter((element) => { return element > 9; });
+ 'splitingForRunningModulo' :           `alphaPosition.filter((element) => { return element > 9; });
 
-alphaPosition.filter((element) => { return element < 10; });`;
+                                         alphaPosition.filter((element) => { return element < 10; });`,
 
 // Section Id 8
-const applyingModuloForNumberNotUnity = 'setModulable.map( element => element % 10 )'
+ 'applyingModuloForNumberNotUnity' :    'setModulable.map( element => element % 10 )',
 
-const scalableArray = `existingUnitiesArray.filter(element => {
-    if(element > 6) {
-        return element;
-    }
-    
-})
-.map(element => element += 3)
-.map(element => element %= 10);`;
+ 'scalableArray' :                      `existingUnitiesArray.filter(element => {
+                                            if(element > 6) {
+                                                return element;
+                                            }
+                                            
+                                        })
+                                        .map(element => element += 3)
+                                        .map(element => element %= 10);`,
 
 // section Id 9
 
-const reassemblyingArrays = `existingUnities.filter(
-    element => {
-        if(element < 7 ) {
-            return element;
-        }
-        
-})
+ 'reassemblyingArrays' :                `existingUnities.filter(
+                                        element => {
+                                            if(element < 7 ) {
+                                                return element;
+                                            }
+                                            
+                                    })
 
-.concat(secondaryArrays, firstArray);`;
+                                    .concat(secondaryArrays, firstArray);`,
 
 // Section id 10
 
-const newString = `const dataSize = data.length
+ 'newString' :                      `const dataSize = data.length
 
-const promo = newArray.join('').concat(dataSize);`
+                                    const promo = newArray.join('').concat(dataSize);`,
 
 // Section id 11
 
-const websiteCompleted = `const promoCode = 'https://formation.yoandev.co/?coupon=' + promo;`
+ 'websiteCompleted' :               `const promoCode = 'https://formation.yoandev.co/?coupon=' + promo;`
+}
+
 
 // Creating an array from all code for exhibiting
 
-const exhibitedCodeArray = [
-        retrievingData,
-        deepDataCopy,
-        mappingAndFiltering, 
-        numberConvertedInString,
-        comparatingDataWithNumberArray,
-        alphabetArrayAndComparaingWithStringArray,
-        splitingForRunningModulo,
-        applyingModuloForNumberNotUnity,
-        scalableArray,
-        reassemblyingArrays,
-        newString,
-        websiteCompleted
-]
+// const exhibitedCodeArray = [
+//         retrievingData,
+//         deepDataCopy,
+//         mappingAndFiltering, 
+//         numberConvertedInString,
+//         comparatingDataWithNumberArray,
+//         alphabetArrayAndComparaingWithStringArray,
+//         splitingForRunningModulo,
+//         applyingModuloForNumberNotUnity,
+//         scalableArray,
+//         reassemblyingArrays,
+//         newString,
+//         websiteCompleted
+// ]
+
+'____'
+
+const codeContent = Object.values(ExhibitingOfCoding);
+
+console.log(codeContent);
 
 
 // GETTING EACH ID FROM EACH SECTION
