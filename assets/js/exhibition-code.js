ff12c87c917615296e30f1703d9f63b65888da5d
OAN <oan@fedora.home>
2023-04-23 15:13:51 +0200

    test : iteration on obj for insert all content and keys in each element of section

--- assets/js/exhibition-code.js
+++ assets/js/exhibition-code.js
@@ -79,7 +79,8 @@ const exhibitingOfCoding = {
 
 '____'
 
-const codeContent = Object.values(exhibitingOfCoding);
+// const codeContent = Object.values(exhibitingOfCoding);
+
 
 // console.log(codeContent);
 
@@ -87,16 +88,57 @@ const codeContent = Object.values(exhibitingOfCoding);
 // Putting the values on the code tag
 
 
-for(let i = 0; i < codeContent.length; i++) {
+// for(let i = 0; i < codeContent.length; i++) {
 
-const preTag = document.getElementsByTagName('pre')
-// console.log(preTag);
-const codeTag = document.getElementsByTagName('code')
-// console.log(codeTag);
-    codeTag.innerHTML = (codeContent[i]);
-}
+// const preTag = document.getElementsByTagName('pre')
+// // console.log(preTag);
+// const codeTag = document.getElementsByTagName('code')
+// // console.log(codeTag);
+//     codeTag.innerHTML = (codeContent[i]);
+// }
 
 
 '----'
 
+// for(const [key, value] of Object.entries(exhibitingOfCoding)) {
+//     console.log(key);
+//     console.log(value);
+// }
+'-----'
 
+const objK = Object.entries(exhibitingOfCoding).map(([key]) => `${key}`)
+const objValue = Object.entries(exhibitingOfCoding).map(([key, value]) => ` ${value}`)
+
+// console.log(objK);
+// console.log(objValue);
+
+// for(let i = 0; i < objValue.length; i++) {
+//     let codeTag =  document.getElementsByTagName('code')
+    
+// } 
+
+'----'
+// for(const [key, value] of Object.entries(exhibitingOfCoding)) {
+//     let codeContentKey = key;
+//     let codeContentobj = value;
+//     let pre = document.getElementsByTagName('pre');
+//     let codeTag = document.getElementsByTagName('code')
+//     codeTag.innerHTML = (codeContentobj);
+//     pre.setAttribute('id', (codeContentKey))
+
+
+    
+// }
+
+'___________'
+
+const sections = document.querySelectorAll('section');
+console.log(sections);
+
+// for( const section of sections) {
+//     let sectionTag = document.getElementsByTagName(section);
+//     let sectionId = document.getElementById('*[id]')
+//     console.log(sectionTag);
+//     console.log(sectionId);
+    
+// }
